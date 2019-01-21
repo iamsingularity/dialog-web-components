42fa14cd9c8e1f9b76c59da655d389e4fe1b3ed1
Matvey Neyman <matvey.neyman@gmail.com>
2019-01-21 20:25:20 +0300

    feat(CustomForm): add more props to support deeper customization

---
+++ src/components/CustomForm/types.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2019 dialog LLC <info@dlg.im>
+ * @flow
+ */
+
+export type ArrayFieldTemplateProps = {
+  title: string,
+  items: Array<*>,
+  onAddClick: () => mixed,
+  canAdd: boolean,
+};
